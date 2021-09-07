2e0748ecce8dfd0bd2657a0ff5d95a66de080d42
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2021-09-07 09:21:11 -0700

    Add the start of Processor and State

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,9 @@ pub enum EscrowError {
   /// Invalid Instruction
   #[error("Invalid Instruction")]
   InvalidInstruction,
+  /// Not Rent Exempt
+  #[error("Escrow Account Not Rent Exempt")]
+  NotRentExempt,
 }
 
 impl From<EscrowError> for ProgramError {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub mod entrypoint;
 pub mod instruction;
+pub mod processor;
+pub mod state;
 pub mod error;
+
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
